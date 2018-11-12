f8376f4cca5eb1f9f5dac300bb2bc4303b4812f3
ConnorKunstek <connorkunstek7@gmail.com>
2018-11-12 15:29:11 -0500

    Adding proposed file/class hierarchy

--- src/Morph.java
+++ src/Morph.java
@@ -2,6 +2,24 @@
 
 //act as overall program driver
 
+/**
+ * proposed file/class hierarchy
+ *
+ * Morph->Frame->Grid->Point->Dot
+ *                   ->GridLine ->Line
+ *             ->Image
+ *
+ * Morph creates Frame
+ * Frame extends JFrame, holds image and grid
+ * Image loads/saves image
+ * Grid creates Points and GridLines
+ * Point holds logic for each individual point (ei control point or not, neighbors)
+ * Dot extends JComponent, holds image of dot for each point
+ * GridLine holds logic for each line between each point
+ * Line extends JComponent, creates actual line image
+ *
+ *
+ */
 
 public class Morph {
 }
