8276f50f0afb2d859e719b8b768c74ff4b854b4c
Nick Sladic <nick.sladic@g.uky.edu>
2018-12-07 16:46:19 -0500

    added GridController

--- src/GridController.java
+++ src/GridController.java
@@ -18,7 +18,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.awt.geom.Line2D;
 import java.awt.event.*;
-import javax.swing.Timer;
 import java.io.File;
 import java.io.IOException;
 import javax.imageio.ImageIO;
@@ -42,9 +41,9 @@ public class GridController extends JPanel{
     private MorphTools MorptT;
 
     private Timer preview;
-    private double tspeed;
+    private double tspeed = 5;
     private double increments;
-    private int frames;
+    private int f = 0;
     private int size;
     private int[][] neighbors ={
             {-1,-1},                //top-right
@@ -130,6 +129,21 @@ public class GridController extends JPanel{
      * @Returns:        NA
      * @Description:
      */
+    public void updateolygons(){
+        for(int i = 0; i < this.size; i++) {
+            for (int j = 0; j < this.size; j++) {
+                poly[i][j][0] = new PolygonController(p[i][j],p[i+1][j],p[i+1][j+1]);
+                poly[i][j][1] = new PolygonController(p[i][j],p[i][j+1],p[i+1][j+1]);
+            }
+        }
+    }
+
+    /*
+     * @Function:       ()
+     * @Parameters:     Type:
+     * @Returns:        NA
+     * @Description:
+     */
     public void setSize(int size) {
         this.size = size;
     }
@@ -191,6 +205,154 @@ public class GridController extends JPanel{
                 }
             }
         }
+    }
+
+    /*
+     * @Function:       ()
+     * @Parameters:     Type:
+     * @Returns:        NA
+     * @Description:
+     */
+    public int[] getCurrentPoint(Point cp){
+        for(int i = 1; i < this.size - 1; i++){
+            for(int j = 1; j < this.size - 1; j++){
+                //checks if the point is inside the polygon
+                if(p[i][j].getModel().contains(cp.x, cp.y)){
+                   int[] clickedPoint = {i,j};
+                   return clickedPoint;
+                }
+            }
+        }
+        return null;
+    }
+
+    /*
+     * @Function:       ()
+     * @Parameters:     Type:
+     * @Returns:        NA
+     * @Description:
+     */
+    public boolean animationStep(PointController bottom[][], PointController top[][], double time){
+        boolean done = false;
+
+        for(int i = 1; i < this.size - 1; i++){
+            for(int j = 1; j < this.size - 1; j++){
+                if(this.increments <= 1){
+                    double nx;
+                    double ny;
+
+                    nx = bottom[i][j].getModel().getX() + (time * (top[i][j].getModel().getX() - bottom[i][j].getModel().getX()));
+                    ny = bottom[i][j].getModel().getY() + (time * (top[i][j].getModel().getY() - bottom[i][j].getModel().getY()));
+                    p[i][j].getModel().setCurrentPos(nx,ny);
+
+                }
+                else{
+                    done = true;
+                }
+            }
+        }
+        return done;
+    }
+
+    public void createAnimation(GridController preI, GridController postI, boolean export){
+        ActionListener s = new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                if(animationStep(preI.p, postI.p, increments)){
+                    preview.start();
+                }
+                else{
+                    increments += 0.006666667;
+                    for(int i = 0; i < preI.poly.length; i++){
+                        for(int j = 0; j < preI.poly[0].length; j++){
+                            MorptT.warpTriangle(preI.image, pre, preI.poly[i][j][0].getModel(), poly[i][j][0].getModel(),null,null);
+                            MorptT.warpTriangle(preI.image, pre, preI.poly[i][j][1].getModel(), poly[i][j][1].getModel(),null,null);
+                            MorptT.warpTriangle(postI.image, post, preI.poly[i][j][0].getModel(), poly[i][j][0].getModel(),null,null);
+                            MorptT.warpTriangle(postI.image, post, preI.poly[i][j][1].getModel(), poly[i][j][1].getModel(),null,null);
+
+                            pre.createGraphics();
+                            if(increments >= 1){
+                                increments = 1;
+                            }
+                            Alpha1 = AlphaComposite.getInstance(AlphaComposite.SRC_OVER, (float)(1-increments));
+
+                            post.createGraphics();
+                            Alpha2 = AlphaComposite.getInstance(AlphaComposite.SRC_OVER, (float)(increments));
+                            if(increments == 1){
+                                increments++;
+                            }
+
+                        }
+                    }
+                }
+
+                repaint();
+                f++;
+
+                if(export){
+
+                }
+            }
+        };
+
+        preview = new Timer(33, s);
+        preview.start();
+    }
+
+    /*
+     * @Function:       ()
+     * @Parameters:     Type:
+     * @Returns:        NA
+     * @Description:
+     */
+    public void setImages(String imgPath1, String imgPath2){
+        try{
+            pre = ImageIO.read(new File(imgPath1));
+            post = ImageIO.read(new File(imgPath1));
+            super.removeAll();
+            super.revalidate();
+            super.repaint();
+        }catch(IOException e){
+            System.out.println("ERROR: Couldn't open file");
+        }
+
+    }
+
+    /*
+     * @Function:       ()
+     * @Parameters:     Type:
+     * @Returns:        NA
+     * @Description:
+     */
+    public boolean getImage(String imgPath){
+        try {
+            this.image = ImageIO.read(new File(imgPath));
+            this.removeAll();
+            this.revalidate();
+            this.repaint();
+            return true;
+        }catch(IOException e){
+            System.out.println("ERROR: Could not open file");
+            return false;
+        }
+    }
+
+    /*
+     * @Function:       ()
+     * @Parameters:     Type:
+     * @Returns:        NA
+     * @Description:
+     */
+    public void changeIntensity(float intensity){
+        RescaleOp operator = new RescaleOp(intensity, 0, null);
+        this.image = operator.filter(this.original,null);
+        repaint();
+    }
+
+    public void resetGrid(){
+        int margin = 500/this.size;
+        setGridSize(this.size);
+        initializePoints(margin);
+        initializePolygons();
     }
 
 }
